f8a18f594c6af68db5b2b7d6181a50ee019a8b5b
crileaech <crileaech@gmail.com>
2016-02-23 12:56:52 +0100

    add cool-ascii-faces in index

---
+++ index.js
@@ -0,0 +1,26 @@
+var cool = require("cool-ascii-faces");
+/*console.log(cool());*/
+
+var express =require("express");
+var app = express(); 
+
+app.get('/',function(req, res){
+	res.send(cool());
+	console.log("New request!!");
+
+});
+
+app.listen(4500);
+
+
+
+/*function calcimc(){
+	var masa = 50.0;
+	
+	var altura  = 1.58;
+	var altura = altura*altura;
+	return   masa / altura ;
+}
+
+console.log(calcimc());
+*/
