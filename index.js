d223d77af9ba2cdadf34a19f1ab2dae947347a61
Victor Grousset <victor@tuxayo.net>
2016-02-27 20:51:02 +0100

    Refactor: Order of function calls

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ var cool = require("cool-ascii-faces");
 
 var express =require("express");
 var app = express(); 
+app.listen("3000");
 
 app.get('/',function(req, res) {
 	res.send(cool());
 });
 
-
 app.get('/about/population',function(req, res) {
 	  res.send("TODO introduice this part of the data");
 });
-
-app.listen("3000");
