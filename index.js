6bd92aefbabaef9285d0a7e309e25c06df35080f
Miguel Angel Cifredo <macifredo@gmail.com>
2016-02-28 17:09:15 +0100

    probando

--- index.js
+++ index.js
@@ -22,40 +22,7 @@ app.get("/about/workers", function(req, res) {
 app.get("/about/olders", function(req, res) {
 	  res.send(v_info_olders);
 });
-/*
-var v_info_population = "
-<p>
-  Data: Population
-</p>
-<p>
-  Collums:  year, province, age, birthplace, number
-</p>
-<p>
-  Description: The number of people from EU and South America, separated in two age ranges (15-19, 20-24), by province, on the years 2014 and 2015.
-</p>
-";
 
-var v_info_workers = "
-<p>
-  Data: Workers
-</p>
-<p>
-  Collums:  province, year, industry, value
-</p>
-<p>
-  Description: Labor market activity, employment and unemployment survey working population.
-</p>
-";
-
-var v_info_olders = "
-<p>
-  Data: Olders
-</p>
-<p>
-  Collums:  yerar, province, men, women
-</p>
-<p>
-  Description: Population 18 years old.
-</p>
-";
-*/
+var v_info_population = "<p>Data: Population</p><p>Collums: year, province, age, birthplace, number</p><p>Description: The number of people from EU and South America, separated in two age ranges (15-19, 20-24), by province, on the years 2014 and 2015.</p>";
+var v_info_workers    = "<p>Data: Workers</p>   <p>Collums: province, year, industry, value</p>        <p>Description: Labor market activity, employment and unemployment survey working population.</p>";
+var v_info_olders     = "<p>Data: Olders</p>    <p>Collums: yerar, province, men, women</p>            <p>Description: Population 18 years old.</p>";
