f54d63883da05b454dae99df2de03a92be5eaa7f
MiguelAngelCifredo <macifredo@gmail.com>
2016-02-29 15:04:02 +0100

    Limpieza del código

--- index.js
+++ index.js
@@ -31,16 +31,10 @@ app.get("/about/olders", function(req, res) {
 });
 
 var v_about           = "";
-// change my name was spelled wrong 
-	
-    v_about           += "<p>Cristina Leal Echevarria: <a href='/about/workers'>workers</a></p>";
+    v_about           += "<p>Cristina Leal Echevarria:     <a href='/about/workers'>workers</a>      </p>";
     v_about           += "<p>Victor Grousset Aburto Duran: <a href='/about/population'>population</a></p>";
-    v_about           += "<p>Miguel Angel Cifredo Campos: <a href='/about/olders'>olders</a></p>";
-//change some  words was spelled wrong
+    v_about           += "<p>Miguel Angel Cifredo Campos:  <a href='/about/olders'>olders</a>        </p>";
+
 var v_info_population = "<p>Data: Population</p><p>Columns: year, province, age, birthplace, number</p><p>Description: The number of people from EU and South America, separated in two age ranges (15-19, 20-24), by province, on the years 2014 and 2015.</p>";
-var v_info_workers    = "<p>Data: Workers </p><p>Columns: province , year, industry, value </p> <p>Description: Labor market activity, employment and unemployment survey working population.</p>";
-var fs =require("fs");
-var dataWorkers = [];
-var content = fs.readFileSync('data-Workers.json','utf-8');
-dataWorkers = JSON.parse(content);
-var v_info_olders     = "<p>Data: Olders</p>    <p>Columns: year, province, men, women</p>            <p>Description: Population 18 years old.</p>";
+var v_info_workers    = "<p>Data: Workers </p>  <p>Columns: province , year, industry, value       </p><p>Description: Labor market activity, employment and unemployment survey working population.</p>";
+var v_info_olders     = "<p>Data: Olders</p>    <p>Columns: year, province, men, women             </p><p>Description: Population 18 years old.</p>";
