9bf69c2d24eb3755f727c64aade27d062018cce0
MiguelAngelCifredo <macifredo@gmail.com>
2016-02-29 15:19:13 +0100

    Limpieza del código

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var express =require("express");
 var app = express(); 
 
@@ -8,34 +7,39 @@ var app = express();
 app.listen(process.env.PORT);
 
 app.get("/", (req,res) => {
-	res.write("<html>");
-	res.write("<head><title>My first application on Heroku</title></head>");
-	res.write("<body><h2>Hello world from sos-2016-02 </h2></body>");
-	res.write("</html>");
-	res.end();
+	res.send(v_html_code);
 });
 
 app.get("/about", function(req, res) {
-	  res.send(v_about);
+	res.send(v_about);
 });
 
 app.get("/about/population", function(req, res) {
-	  res.send(v_info_population);
+	res.send(v_info_population);
 });
 
 app.get("/about/workers", function(req, res) {
-	  res.send(v_info_workers);
+	res.send(v_info_workers);
 });
 
 app.get("/about/olders", function(req, res) {
-	  res.send(v_info_olders);
+	res.send(v_info_olders);
 });
 
-var v_about           = "";
+var v_html_code        = "";
+	v_html_code       += "<html>";
+	v_html_code       += "<head><title>My first application on Heroku</title></head>";
+	v_html_code       += "<body>";
+	v_html_code       += "<h2>Hello world from sos-2016-02 </h2>";
+	v_html_code       += "<p><a href='/about'>about</a></p>";
+	v_html_code       += "</body>";
+	v_html_code       += "</html>";	
+
+var v_about            = "";
     v_about           += "<p>Cristina Leal Echevarria:     <a href='/about/workers'>workers</a>      </p>";
     v_about           += "<p>Victor Grousset Aburto Duran: <a href='/about/population'>population</a></p>";
     v_about           += "<p>Miguel Angel Cifredo Campos:  <a href='/about/olders'>olders</a>        </p>";
 
-var v_info_population = "<p>Data: Population</p><p>Columns: year, province, age, birthplace, number</p><p>Description: The number of people from EU and South America, separated in two age ranges (15-19, 20-24), by province, on the years 2014 and 2015.</p>";
-var v_info_workers    = "<p>Data: Workers </p>  <p>Columns: province , year, industry, value       </p><p>Description: Labor market activity, employment and unemployment survey working population.</p>";
-var v_info_olders     = "<p>Data: Olders</p>    <p>Columns: year, province, men, women             </p><p>Description: Population 18 years old.</p>";
+var v_info_population  = "<p>Data: Population</p><p>Columns: year, province, age, birthplace, number</p><p>Description: The number of people from EU and South America, separated in two age ranges (15-19, 20-24), by province, on the years 2014 and 2015.</p>";
+var v_info_workers     = "<p>Data: Workers </p>  <p>Columns: province , year, industry, value       </p><p>Description: Labor market activity, employment and unemployment survey working population.</p>";
+var v_info_olders      = "<p>Data: Olders</p>    <p>Columns: year, province, men, women             </p><p>Description: Population 18 years old.</p>";
