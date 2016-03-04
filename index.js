c20042d8d8805d07949a95d60bb8e0696c3de447
Victor Grousset/tuxayo <victor@tuxayo.net>
2016-03-04 18:49:17 +0100

    Clean static files routing #31

--- index.js
+++ index.js
@@ -1,44 +1,16 @@
-var express =require("express");
+var express = require("express");
 var app = express();
 var port = (process.env.PORT || 3000);
 
-app.use("/", express.static(__dirname + "/static"));
+app.use("/", express.static(__dirname + "/static", {'extensions': ['html']}));
 
 app.listen(port, () => {
 	console.log("Web server is running and listening on port: " + port);
 });
 
-app.get("/", (req, res) => {
-	res.send(redirect("index.html"));
-});
-
-
 app.get("/time", (req, res) => {
 	res.send(getFecha());
 });
-
-app.get("/about", function(req, res) {
-	res.send(redirect("about.html"));
-});
-
-
-app.get("/about/population", function(req, res) {
-	res.send(redirect("population.html"));
-});
-
-
-app.get("/about/workers", function(req, res) {
-	res.send(redirect("workers.html"));
-});
-
-
-app.get("/about/olders", function(req, res) {
-	res.send(redirect("olders.html"));
-});
-
-function redirect(pageName){
-	return "<script>window.location.href='/" + pageName + "';</script>";
-};
 
 function getFecha(){
 	var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
@@ -50,4 +22,4 @@ function getFecha(){
 	var segundos = (s < 10) ? '0' + s : s;
 	var ret = diasSemana[f.getDay()] + ", " + f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear() + " [" + f.getHours() + ":" + minutos + ":" + segundos + "]";
 	return ret;
-};
+}
