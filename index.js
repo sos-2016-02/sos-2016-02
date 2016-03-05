078dbfc824fd335e4cd94ab134ec948f6035bc3a
Crileaech <crileaech@gmail.com>
2016-03-05 11:18:30 +0100

    modify the code of date. Closes #34

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ app.get("/", (req, res) => {
 	res.send(redirect("index.html"));
 });
 
-
+//modify getFecha
 app.get("/time", (req, res) => {
-	res.send(getFecha());
+	res.send(getFecha()=>{
+		console.log(getFecha);	
+	});
 });
 
 app.get("/about", function(req, res) {
@@ -40,7 +42,7 @@ function redirect(pageName){
 	return "<script>window.location.href='/" + pageName + "';</script>";
 };
 
-function getFecha(){
+/*function getFecha(){
 	var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
 	var diasSemana = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
 	var f = new Date();
@@ -50,4 +52,37 @@ function getFecha(){
 	var segundos = (s < 10) ? '0' + s : s;
 	var ret = diasSemana[f.getDay()] + ", " + f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear() + " [" + f.getHours() + ":" + minutos + ":" + segundos + "]";
 	return ret;
+};*/
+
+// propose this code for date 
+// what do you think guys ??
+function getFecha(){
+	
+var d=new Date();
+var dia=new Array(7);
+dia[0]="Domingo";
+dia[1]="Lunes";
+dia[2]="Martes";
+dia[3]="Miercoles";
+dia[4]="Jueves";
+dia[5]="Viernes";
+dia[6]="Sabado";
+var m = d.getMonth() + 1;
+var mesok = (m < 10) ? '0' + m : m;
+var mesok=new Array(12);
+mesok[0]="Enero";
+mesok[1]="Febrero";
+mesok[2]="Marzo";
+mesok[3]="Abril";
+mesok[4]="Mayo";
+mesok[5]="Junio";
+mesok[6]="Julio";
+mesok[7]="Agosto";
+mesok[8]="Septiembre";
+mesok[9]="Octubre";
+mesok[10]="Noviembre";
+mesok[11]="Diciembre";
+document.write("Hoy es: " + dia[d.getDay()] "Este mes es: " + mesok[mm.getMonth()]);
+
 };
+
