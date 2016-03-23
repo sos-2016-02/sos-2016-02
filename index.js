cdcad59965683860d55408dc91179e14b40e852d
Miguel Angel Cifredo <macifredo@gmail.com>
2016-03-23 17:15:42 +0100

    Modifing index.js to work

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var api_films               = require('./api_films');
 var api_linux_distributions = require('./api_linux_distributions');
 
 // __ VAR -> API's Version 1 __
-// var api_olders              = require('./api_olders');
-// var api_workers             = require('./api_workers');
+var api_olders              = require('./api_olders');
+var api_workers             = require('./api_workers');
 var api_population          = require('./api_population');
 
 // __ VAR -> Others __
@@ -21,8 +21,8 @@ app.use('/api/sandbox/films', api_films);
 app.use('/api/sandbox/linux-distributions', api_linux_distributions);
 
 // __ URI -> API's Version 1 __
-// app.use('/api/v1/olders', api_olders);
-// app.use('/api/v1/workers', api_workers);
+app.use('/api/v1/olders', api_olders);
+app.use('/api/v1/workers', api_workers);
 app.use('/api/v1/population', api_population);
 
 // __ Other Parameters __
