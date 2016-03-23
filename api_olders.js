c1679a0efec0c729b18955015b3be3a2ae9dc736
Miguel Angel Cifredo <macifredo@gmail.com>
2016-03-23 17:19:11 +0100

    Modifing apis from missgin files to return router status

---
+++ api_olders.js
@@ -0,0 +1,10 @@
+// ============================================================================ VARIABLES DEFINITION
+var tools      = require('./tools');
+var bodyParser = require('body-parser');
+var express    = require('express');
+var router     = express.Router();
+
+router.use(bodyParser.json());
+
+
+module.exports = router;
