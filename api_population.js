8f3a56cc5c7fb784c537fd7380ec0c4a5da5d1fe
Victor Grousset/tuxayo <victor@tuxayo.net>
2016-04-03 12:36:37 +0200

    API: population: move auth code to controller, close #62

--- api_population.js
+++ api_population.js
@@ -1,39 +1,20 @@
-var controller = require('./controllers/api/population.js');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 var express = require('express');
 var router = express.Router();
+var controller = require('./controllers/api/population.js');
 
-var passport = require('passport');
-LocalAPIKeyStrategy = require('passport-localapikey-update').Strategy;
+module.exports = router;
 
 router.use(bodyParser.json());
 router.use(passport.initialize());
 
-passport.use(new LocalAPIKeyStrategy(
-    function(apikey, done) {
-        if (apikey == 'correct-key-1' ||
-            apikey == 'correct-key-2') {
-            return done(null, true);
-        } else {
-            return done(null, false);
-        }
-    }
-));
-
-function checkAuthentication(req, res, next) {
-    passport.authenticate('localapikey', function(err, user, info) {
-        if (user == false) {
-            return res.sendStatus(401);
-        }
-        return next();
-    })(req, res, next);
-};
 
 router.get('/loadInitialData', (req,res) => controller.loadInitialData(req, res) );
 
 // on collection
 router.get('/', (req, res) => controller.getPopulationData(req, res));
-router.post('/', checkAuthentication, (req, res) => controller.postNewDatum(req, res));
+router.post('/', controller.checkAuthentication, (req, res) => controller.postNewDatum(req, res));
 router.put('/', (req, res) => res.sendStatus(405));
 router.delete('/', (req, res) => controller.deleteAllData(req, res));
 
@@ -46,5 +27,3 @@ router.get('/:province/:year', (req, res) => controller.getDatum(req, res));
 router.put('/:province/:year', (req, res) => controller.putDatumToUpdate(req, res));
 router.delete('/:province/:year', (req, res) => controller.deleteDatum(req, res));
 router.post('/:province/:year', (req, res) => res.sendStatus(405));
-
-module.exports = router;
