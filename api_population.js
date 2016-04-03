cf340355fa9faee70965a32de76493e66eb6c3b3
Victor Grousset/tuxayo <victor@tuxayo.net>
2016-04-03 13:11:39 +0200

    API: population: requirer auth for every request

--- api_population.js
+++ api_population.js
@@ -10,20 +10,20 @@ router.use(bodyParser.json());
 router.use(passport.initialize());
 
 
-router.get('/loadInitialData', (req,res) => controller.loadInitialData(req, res) );
+router.get('/loadInitialData', controller.checkAuthentication, (req,res) => controller.loadInitialData(req, res) );
 
 // on collection
-router.get('/', (req, res) => controller.getPopulationData(req, res));
+router.get('/', controller.checkAuthentication, (req, res) => controller.getPopulationData(req, res));
 router.post('/', controller.checkAuthentication, (req, res) => controller.postNewDatum(req, res));
-router.put('/', (req, res) => res.sendStatus(405));
-router.delete('/', (req, res) => controller.deleteAllData(req, res));
+router.put('/', controller.checkAuthentication, (req, res) => res.sendStatus(405));
+router.delete('/', controller.checkAuthentication, (req, res) => controller.deleteAllData(req, res));
 
 // on a subset of collection
-router.get('/:province(\\D+)/', (req, res) => controller.getDataByProvince(req, res));
-router.get('/:year(\\d+)/', (req, res) => controller.getDataByYear(req, res));
+router.get('/:province(\\D+)/', controller.checkAuthentication, (req, res) => controller.getDataByProvince(req, res));
+router.get('/:year(\\d+)/', controller.checkAuthentication, (req, res) => controller.getDataByYear(req, res));
 
 // on individual ressources
-router.get('/:province/:year', (req, res) => controller.getDatum(req, res));
-router.put('/:province/:year', (req, res) => controller.putDatumToUpdate(req, res));
-router.delete('/:province/:year', (req, res) => controller.deleteDatum(req, res));
-router.post('/:province/:year', (req, res) => res.sendStatus(405));
+router.get('/:province/:year', controller.checkAuthentication, (req, res) => controller.getDatum(req, res));
+router.put('/:province/:year', controller.checkAuthentication, (req, res) => controller.putDatumToUpdate(req, res));
+router.delete('/:province/:year', controller.checkAuthentication, (req, res) => controller.deleteDatum(req, res));
+router.post('/:province/:year', controller.checkAuthentication, (req, res) => res.sendStatus(405));
