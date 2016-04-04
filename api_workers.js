2721faf7456bd2e63351bf79d3ff5d940ec1ad59
Crileaech <crileaech@gmail.com>
2016-04-04 13:33:34 +0200

    add code to api_workers check by two properties. Closes #66

--- api_workers.js
+++ api_workers.js
@@ -10,7 +10,6 @@ router.use(bodyParser.json());
 var workers = [];
 
 
-
 // Cargo datos Iniiciales
 
 router.get('/loadInitialData',function(req,res){
@@ -35,6 +34,13 @@ router.get('/', (req, res) => {
 
 
 router.post('/', (req, res) => {
+	var apikey = req.query.apikey;
+	if(!(apikey && apikey=="api_key_id")){
+		res.sendStatus(403) // forbiden
+	}else{
+		res.send(200);
+		res.send(workers);
+	}
     var content = req.body;
     workers.push(content);
     res.sendStatus(201);
@@ -56,56 +62,81 @@ router.delete('/', (req, res) => {
 
 // POST insertar nuevo recurso 
 
-router.post('/:industry', (req,res) => {
+router.post('/:year/:industry', (req,res) => {
 	res.sendStatus(405);
 });
 
 // GET para un identificador
-router.get('/:industry', (req,res) => {
+router.get('/:industry(\\D+/)', (req,res) => {
 	var industryValue = req.params.industry;
-
-	/*hacer un for recorre todo los datos 
-	si el dato.industry == industryValue lo meto en un array nuevo que he creado previamente vacio. Devuelvo 
-	
-	despues del for el array con todos los recursos que cumplen la condicion.*/
-	
-	if (industryValue == "loadInitialData") {
-		workers = loadInitialData();
-		res.sendStatus(200);
-	} else {
-		var item = tools.findByAttr(workers,'industry',industryValue);
-		if (item == null) {
-			res.sendStatus(404);
-		} else {
-			res.send(item);
-		}
+	var industryData = tools.findAllByProperty(workers,'industry',industry);
+	if(req.query.minIndustry !=undefined){
+		workers = workers.filter((content)=>{
+			return content ['number'] >= req.query..minIndustry;
+		});
 	}
+	res.send(workers);
+});
+	/*hacer un for recorre todo los datos 
+	si el dato.industry == industryValue lo meto en un array nuevo 
+	que he creado previamente vacio. 
+	Devuelvo despues del for el array con 
+	todos los recursos que cumplen la condicion.*/
+
+//GET busca recurso por año 
+
+router.get('/:year(\\d+)/',function(req,res){
+	var year =req.params.year;
+	var content = tools.findAllByProperty(workers,'year',year);
+	res.send(content);
 });
 
-//PUT actualiza un registro already exist
 
-router.put('/:industry', (req,res) => {
-	var industryValue = req.params.industry;
-	var statusCode;
-	if (tools.removeByAttr(workers,'industry',industryValue) == 0) {
-		statusCode = 404;
-	} else {
-		var worker = req.body;
-		workers.push(worker);
-		statusCode = 200;
+// GET busca por dos ecursos determinados 
+
+
+router.get('/:year/:industry', function(req,res){
+	var year =req.params.year;
+	var industry = req.params.industry;
+	var findAllByYear = tools.findAllByProperty(workers,'year',year);
+	var content = tools.findAllByProperty(findAllByYear,'industry',industry);
+	if(content== undefined){
+		res.sendStatus(404);
+	}else{
+		res.send(content);
 	}
-	res.sendStatus(statusCode);
+});
+	
+
+//PUT actualiza un registro already exist by to id
+
+
+router.put('/:year/:industry', (req,res) => {
+	var industryValue = req.params.industry;
+	
+	if (tools.removeByTwoProperties(workers,'industry',req.params.industry,
+									'year',req.params.year)){
+		content = req.body;
+		workers.push(content);
+		res.sendStatus(200);
+	} else {
+		
+		res.sendStatus(404);
+	}
+	
 });
 
 //DELETE borramos un trabajador con un especifico id 
 
-router.delete('/:industry', (req,res) => {
-	var industryValue = req.params.industry;
+
+
+router.delete('/:year/:industry', (req,res) => {
 	var statusCode;
-	if (tools.removeByAttr(workers,'industry',industryValue) == 0) {
-		statusCode = 404;
-	} else {
+	if (tools.removeByTwoProperties(workers,'year',req.params.year,
+									'industry',req.params.industry)) {
 		statusCode = 200;
+	} else {
+		statusCode = 404;
 	}
 	res.sendStatus(statusCode);
 });
