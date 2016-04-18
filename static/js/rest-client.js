00b240dd856a1480eaaf659bb204eb17354f6535
Miguel Angel Cifredo <macifredo@gmail.com>
2016-04-19 00:21:49 +0200

    Extraer el JavaScript en otro fichero del API-Rest de Olders

--- static/js/rest-client.js
+++ static/js/rest-client.js
@@ -1,25 +1,27 @@
 $(document).ready(function(){
 	  $("#btnSend").click(function(){
-		    $("#strResponseRaw").html( "" );
-		    $("#strResponsePrettyPrinted").html( "" );
-		    var request = $.ajax({
-			      url        : $("#txtRequestUrl").val()
-			      ,type       : $("input[type=radio]:checked").attr("id")
-			      ,data       : $("#txtRequestPayload").val()
-			      ,contentType: "application/json"
-		    });
-		    request.done(function(data, status, jqXHR){
-			      $("#strResponseStatus").text("");
-            $("#strResponseRaw").text( JSON.stringify(data) );
-			      $("#strResponsePrettyPrinted").html( prettyPrint(data) );
-		    });;
+			$("#strResponseRaw").html( "" );
+			$("#strResponsePrettyPrinted").html( "" );
 
-		    request.always(function(jqXHR, status){
-			      if (status == "success")
-				        $("#strResponseStatus").text(status);
-			      else
-				        $("#strResponseStatus").text(jqXHR.status + " " + jqXHR.statusText);
-		    });;
+			var request = $.ajax({
+					 url        : $("#txtRequestUrl").val()
+					,type       : $("input[type=radio]:checked").attr("id")
+					,data       : $("#txtRequestPayload").val()
+					,contentType: "application/json"
+			});
 
-	  });
+			request.done(function(data, status, jqXHR){
+				$("#strResponseStatus").text("");
+				$("#strResponseRaw").text( JSON.stringify(data) );
+				$("#strResponsePrettyPrinted").html( prettyPrint(data) );
+			});;
+
+			request.always(function(jqXHR, status){
+				if (status == "success")
+					$("#strResponseStatus").text(status);
+				else
+					$("#strResponseStatus").text(jqXHR.status + " " + jqXHR.statusText);
+			});;
+
+		});
 });
