4112f145468313ea3d14ae4b83bfab31d9e68608
Miguel Angel Cifredo <macifredo@gmail.com>
2016-04-24 18:27:32 +0200

    Formaterar la tabla de datos.Closes #77

--- static/js/olders-data-table.js
+++ static/js/olders-data-table.js
@@ -5,37 +5,15 @@ $(document).ready(function(){
 	var vConnection;
 
 	setDefaultValues();
-
 	obtenerDireccion();
+	sendQuery();
+
+	$("#btnLoadInitialData").click(function(){
+		LoadInitialData();
+	});
 
 	$("#btnEnviar").click(function(){
-		$("#txtReceived").html( "" );
-		var vType     = $("input[type=radio]:checked").attr("id");
-		var vDataJSON = $("#txtData").val();
-
-		var request = $.ajax({
-			 url        : vConnection
-			,type       : vType
-			,data       : vDataJSON
-			,contentType: "application/json; charset=utf-8"
-			,cache      : false
-		});
-
-		request.done(function(data, status, jqXHR){
-			$("#txtStatus").text("");
-			//$("#txtReceived").text( JSON.stringify(data) );
-			var table = $.makeTable(data);
-			$(table).appendTo("#txtReceived");
-		});;
-
-		request.always(function(jqXHR, status){
-			if (status == "success"){
-				$("#txtStatus").text("Ok.");
-			} else {
-				$("#txtStatus").text("ERROR " + jqXHR.status + " " + jqXHR.statusText);	
-			}
-		});;
-
+		sendQuery();
 	});
 
 	$("input[name=txtURL]").keyup(function(){
@@ -64,46 +42,113 @@ $(document).ready(function(){
 		$('input[name=txtURL]').val("");
 		// https://sos-2016-02.herokuapp.com
 		// http://localhost:3000
-		$('#txtServer').val("https://sos-2016-02.herokuapp.com");
+		// http://192.168.1.200:3000
+		$('#txtServer').val("http://192.168.1.200:3000");
 		$('#txtURI').val("");
 		$('#txtKey').val("keyRead");
 	}
 
 	function obtenerDireccion() {
-		vAPIname    = $("#selAPIname option:selected").val();
-		vAPIversion = $("#selAPIversion option:selected").val();
-		vConnection = $("#txtServer").val() + "/api/" + vAPIversion + "/" + vAPIname + "/" + $("#txtURI").val();
-		if ($("#apikey").val() != ""){
-			var vChar  = ( vConnection.indexOf("?")==-1) ? "?" : "&";
-			vConnection = vConnection + vChar + "apikey=" + $("#txtKey").val()
-		}
-		if ($("#txtOffset").val() != ""){
-			var vChar  = ( vConnection.indexOf("?")==-1) ? "?" : "&";
-			vConnection = vConnection + vChar + "offset=" + $("#txtOffset").val()
-		}
-		if ($("#txtLimit").val() != ""){
-			var vChar  = ( vConnection.indexOf("?")==-1) ? "?" : "&";
-			vConnection = vConnection + vChar + "limit=" + $("#txtLimit").val()
-		}
-		if ($("#txtFrom").val() != ""){
-			var vChar  = ( vConnection.indexOf("?")==-1) ? "?" : "&";
-			vConnection = vConnection + vChar + "from=" + $("#txtFrom").val()
-		}
-		if ($("#txtTo").val() != ""){
-			var vChar  = ( vConnection.indexOf("?")==-1) ? "?" : "&";
-			vConnection = vConnection + vChar + "to=" + $("#txtTo").val()
-		}
-		if ($("#txtFields").val() != ""){
-			var vChar  = ( vConnection.indexOf("?")==-1) ? "?" : "&";
-			vConnection = vConnection + vChar + "fields=" + $("#txtFields").val()
-		}
-		if ($("#txtValues").val() != ""){
-			var vChar  = ( vConnection.indexOf("?")==-1) ? "?" : "&";
-			vConnection = vConnection + vChar + $("#txtValues").val()
-		}
-
+		vConnection = obtenerURLBase() + obtenerParametros();
 		$('#lnkRequest').attr('href',vConnection);
 		$('#lnkRequest').text(vConnection);
+	}
+
+	function obtenerURLBase() {
+		vAPIname    = $("#selAPIname option:selected").val();
+		vAPIversion = $("#selAPIversion option:selected").val();
+		vURLBase    = $("#txtServer").val() + "/api/" + vAPIversion + "/" + vAPIname + "/" + $("#txtURI").val();
+		return vURLBase;		
+	}
+
+	function obtenerParametros(){
+		var vParam = "";
+		if ($("#apikey").val() != ""){
+			var vChar  = ( vParam.indexOf("?")==-1) ? "?" : "&";
+			vParam = vParam + vChar + "apikey=" + $("#txtKey").val()
+		}
+		if ($("#txtOffset").val() != ""){
+			var vChar  = ( vParam.indexOf("?")==-1) ? "?" : "&";
+			vParam = vParam + vChar + "offset=" + $("#txtOffset").val()
+		}
+		if ($("#txtLimit").val() != ""){
+			var vChar  = ( vParam.indexOf("?")==-1) ? "?" : "&";
+			vParam = vParam + vChar + "limit=" + $("#txtLimit").val()
+		}
+		if ($("#txtFrom").val() != ""){
+			var vChar  = ( vParam.indexOf("?")==-1) ? "?" : "&";
+			vParam = vParam + vChar + "from=" + $("#txtFrom").val()
+		}
+		if ($("#txtTo").val() != ""){
+			var vChar  = ( vParam.indexOf("?")==-1) ? "?" : "&";
+			vParam = vParam + vChar + "to=" + $("#txtTo").val()
+		}
+		if ($("#txtFields").val() != ""){
+			var vChar  = ( vParam.indexOf("?")==-1) ? "?" : "&";
+			vParam = vParam + vChar + "fields=" + $("#txtFields").val()
+		}
+		if ($("#txtValues").val() != ""){
+			var vChar  = ( vParam.indexOf("?")==-1) ? "?" : "&";
+			vParam = vParam + vChar + $("#txtValues").val()
+		}
+		return vParam;
+	}
+
+	function sendQuery(){
+		$("#txtReceived").html( "" );
+		var vType     = $("input[type=radio]:checked").attr("id");
+		var vDataJSON = $("#txtData").val();
+
+		var request = $.ajax({
+			 url        : vConnection
+			,type       : vType
+			,data       : vDataJSON
+			,contentType: "application/json; charset=utf-8"
+			,cache      : false
+		});
+
+		request.done(function(data, status, jqXHR){
+			$("#txtStatus").text("");
+			//$("#txtReceived").text( JSON.stringify(data) );
+			var table = $.makeTable(data);
+			$(table).appendTo("#txtReceived");
+		});;
+
+		request.always(function(jqXHR, status){
+			if (status == "success"){
+				$("#txtStatus").text("Ok.");
+			} else {
+				$("#txtStatus").text("ERROR " + jqXHR.status + " " + jqXHR.statusText);	
+			}
+		});;
+	}
+
+	function LoadInitialData(){
+		$("#txtReceived").html( "" );
+		var vType     = $("input[type=radio]:checked").attr("id");
+		var vDataJSON = $("#txtData").val();
+		var vURL      = obtenerURLBase() + "loadInitialData?apikey=" + $("#txtKey").val();
+
+		var request = $.ajax({
+			 url        : vURL
+			,type       : "GET"
+			,data       : ""
+			,contentType: "application/json; charset=utf-8"
+			,cache      : false
+		});
+
+		request.done(function(data, status, jqXHR){
+			$("#txtStatus").text("TO PERFE");
+		});;
+
+		request.always(function(jqXHR, status){
+			if (status == "success"){
+				$("#txtStatus").text("Ok.");
+				$("#txtReceived").text("Loading Initial Data...   Ok!");
+			} else {
+				$("#txtStatus").text("ERROR " + jqXHR.status + " " + jqXHR.statusText);	
+			}
+		});;
 	}
 
 	$.makeTable = function (dataJSON) {
