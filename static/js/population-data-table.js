292b9b1b251c3d4869bef089920ea895b7ad6d49
Victor Grousset/tuxayo <victor@tuxayo.net>
2016-04-27 22:07:08 +0200

    population GUI: add server side search, closes #80

--- static/js/population-data-table.js
+++ static/js/population-data-table.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#population-data-table").DataTable( {
+    var table = $("#population-data-table").DataTable({
         "ajax": {
             "url": "/api/v1/population?apikey=correct-key-1",
             "dataSrc": ""
@@ -11,6 +11,10 @@ $(document).ready(function() {
             { "data": "birthplace" },
             { "data": "number" },
         ]
-    } );
-
+    });
+    $("#button-search").click(() => {
+        var searchQuery = $("#server-side-search-input").val();
+        var newDataUrl = "/api/v1/population/" + searchQuery +"?apikey=correct-key-1";
+        table.ajax.url(newDataUrl).load();
+    });
 });
