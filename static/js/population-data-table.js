773521dab5aaffcb6e9b7628bb90b1739eb04b24
Victor Grousset/tuxayo <victor@tuxayo.net>
2016-04-29 14:50:23 +0200

    population GUI: add delete

--- static/js/population-data-table.js
+++ static/js/population-data-table.js
@@ -1,5 +1,10 @@
+var API_POPULATION_URL = "/api/v1/population";
+// TODO find in doc how to get a DataTable object from
+// an existing one
+var dataTable;
+
 $(document).ready(function() {
-    var table = $("#population-data-table").DataTable({
+    dataTable = $("#population-data-table").DataTable({
         "ajax": {
             "url": "/api/v1/population?apikey=correct-key-1",
             "dataSrc": ""
@@ -16,7 +21,7 @@ $(document).ready(function() {
     $("#button-search").click(() => {
         var searchQuery = $("#server-side-search-input").val();
         var newDataUrl = "/api/v1/population/" + searchQuery +"?apikey=correct-key-1";
-        table.ajax.url(newDataUrl).load();
+        dataTable.ajax.url(newDataUrl).load();
     });
 
     $("#datum-form").submit((event) => {
@@ -39,7 +44,7 @@ $(document).ready(function() {
         });
 
         request.done(function (response, textStatus, jqXHR){
-            table.ajax.reload();
+            dataTable.ajax.reload();
         });
 
         request.fail(function (jqXHR, textStatus, errorThrown){
@@ -53,10 +58,70 @@ $(document).ready(function() {
             // Reenable the inputs
             $inputs.prop("disabled", false);
         });
-
-
     });
+
+
+    var table = document.getElementById("population-data-table");
+    addActionColumnToHeader(table);
+    $(table).on("draw.dt", function () {
+        addActionButtonsToEachRow(table);
+    } );
 });
+
+
+function addActionColumnToHeader(table) {
+    var header = table.tHead.rows[0];
+    header.innerHTML += "<th>Actions</th>";
+}
+
+function addActionButtonsToEachRow(table) {
+    var tableBody = table.tBodies[0];
+
+    var noData = tableBody.rows[0].cells[0].className == "dataTables_empty";
+    if (noData) return;
+
+    for (var i = 0, row; row = tableBody.rows[i]; i++) {
+        addDeleteButton(row, i);
+    }
+}
+
+function addDeleteButton(row, rowIndex) {
+    buttonId = "delete-button-row-" + rowIndex;
+    var button = "<button id=" + buttonId + ">Delete</button>";
+    row.innerHTML += "<td>" + button + "</td>";
+
+    $("#" + buttonId).click(row, deleteDatumListener);
+}
+
+function deleteDatumListener(event) {
+    row = event.data;
+    var year = row.cells[0].innerHTML;
+    var province = row.cells[1].innerHTML;
+    url = API_POPULATION_URL + "/" + province + "/" + year + "?apikey=" + getApiKey();
+
+    $(event.target).prop("disabled", true);
+    var request = $.ajax({
+        url: url,
+        type: "delete",
+				contentType: "application/json"
+    });
+
+    request.done(function (response, textStatus, jqXHR){
+        dataTable.ajax.reload();
+    });
+
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        console.error(
+            "The following error occurred: "+
+                textStatus, errorThrown
+        );
+    });
+
+}
+
+function getApiKey() {
+    return "correct-key-1";
+}
 
 // http://stackoverflow.com/a/1186309/3682839
 $.fn.serializeObject = function() {
