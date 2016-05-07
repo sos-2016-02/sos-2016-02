03f3e0398deb4970fd579ab787a6363d1dd342de
Victor Grousset/tuxayo <victor@tuxayo.net>
2016-05-07 23:20:53 +0200

    population visualization: add tooltip to show name and population

--- static/js/population/visualization.js
+++ static/js/population/visualization.js
@@ -1,52 +1,73 @@
 // d is the common name of a datum
 // g is and SVG element it's a container used to group other SVG elements
-var width = 960,
-    height = 500;
+
+var provinceToPopulationMap;
+
+var WIDTH = 960,
+    HEIGHT = 500;
 
 var projection = d3.geo.mercator()
         .scale(2300)
         .center([0, 40])
-        .translate([width / 2, height / 2]);
+        .translate([WIDTH / 2, HEIGHT / 2]);
 
 var path = d3.geo.path()
         .projection(projection);
 
 var svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height);
+        .attr("width", WIDTH)
+        .attr("height", HEIGHT);
 queue()
     .defer(d3.json, "/data/population/spain_with_provinces.topojson.json")
     .defer(d3.json, "/data/population/population.json")
     .await(ready);
 
-var provinceNameToPopulationMap;
 function ready(error, es, population) {
     if (error) throw error;
 
-    provinceNameToPopulationMap = {};
-    population.forEach((d) => { provinceNameToPopulationMap[d.province] = d.number; });
+    provinceToPopulationMap = {};
+    population.forEach((d) => { provinceToPopulationMap[d.province] = d.number; });
 
-    console.log(provinceNameToPopulationMap);
+    console.log(provinceToPopulationMap);
     svg.append("g")
         .attr("class", "provinces")  // g node will have this class
         .selectAll("path")
           .data(topojson.feature(es, es.objects.provincias).features)
         .enter().append("path")
           .attr("d", path)
-        .style("fill", function(d) {
-            return colorFromPopulation(d, provinceNameToPopulationMap);
-        });
-
+        .style("fill", (d) => {
+            return colorFromPopulation(d, provinceToPopulationMap);
+        })
+        .on("mouseover", mouseOver)
+        .on("mousemove", mouseMove)
+        .on("mouseout", mouseOut);
 }
 
+var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("display", "none");
+
+function mouseMove(d) {
+    var province = d.properties.nombre;
+    var population = provinceToPopulationMap[province];
+    if (population == undefined) population = "N/A";
+
+    div.text(province + ": " + population)
+        .style("left", (d3.event.pageX - 34) + "px")
+        .style("top", (d3.event.pageY - 12) + "px");
+}
+
+function mouseOver() { div.style("display", "inline"); }
+function mouseOut() { div.style("display", "none"); }
+
 function colorFromPopulation(province, populationData) {
-    population = provinceNameToPopulationMap[province.properties.nombre];
+    population = provinceToPopulationMap[province.properties.nombre];
     if (population == undefined) return '#989898'; // no data
-    return colorQuantizer(population);
+    return quantize(population);
 }
 
 // maps arbitrary slices of a continuous domain to discrete values in the range
-var colorQuantizer = d3.scale.threshold()
+var quantize = d3.scale.threshold()
 // data range is from 1000 to 2000
         .domain([1000, 1250, 1500, 1750, 2000])
         .range(["#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#756bb1", "#54278f"]);
