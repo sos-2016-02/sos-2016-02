018fe935a9dcd1d848c5b1d3dcb55b0ac0068fee
Miguel Angel Cifredo <macifredo@gmail.com>
2016-05-19 21:05:56 +0200

    Reestrutured index page

--- static/js/olders/charts.js
+++ static/js/olders/charts.js
@@ -1,8 +1,8 @@
 var vAPIversion = "v1";
 var vAPIname    = "olders";
 //var vServer     = "http://192.168.1.200:3000";
-//var vServer     = "http://localhost:3000";
-var vServer     = "https://sos-2016-02.herokuapp.com";
+var vServer     = "http://localhost:3000";
+//var vServer     = "https://sos-2016-02.herokuapp.com";
 
 var vApiKeyRead = "keyRead";
 
