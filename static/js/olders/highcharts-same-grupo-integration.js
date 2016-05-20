b6f3c34899cd3a649bb7ddaba12472322a2bff74
Miguel Angel Cifredo <macifredo@gmail.com>
2016-05-20 12:50:08 +0200

    Updating integrations

---
+++ static/js/olders/highcharts-same-grupo-integration.js
@@ -0,0 +1,133 @@
+//var vServer     = "http://192.168.1.200:3000";
+//var vServer     = "http://localhost:3000";
+var vServer     = "https://sos-2016-02.herokuapp.com";
+var vURL        = "";
+var vApiKeyLSA  = "multiPlan_C4_sos-2016-02-mac_ag";
+var vApiKeyOUT  = "correct-key-1";
+
+var vDataCategories_serie1 = [];
+var vDataCategories_serie2 = [];
+var vDataSeries            = [];
+
+$(document).ready(function() {
+  getAllData_serie1("2015");
+});
+
+function getAllData_serie1(pQuery) {
+  var vURL    = vServer + "/api/v1/olders/" + pQuery + "?apikey=" + vApiKeyLSA;
+
+  var request = $.ajax({
+     url        : vURL
+    ,type       : "GET"
+    ,data       : ""
+    ,contentType: "application/json; charset=utf-8"
+    ,cache      : false
+  });
+
+  request.done(function(data, status, jqXHR){
+    getData_serie1(data);
+    getAllData_serie2("loadInitialData");
+    getAllData_serie2("2015");
+  });
+}
+
+function getData_serie1(data){
+  var vMen   = [];
+
+  $.each(data, function(){
+      vDataCategories_serie1.push(this.province);
+      vMen.push(this.men);
+  });
+
+  var vDataMen  = {};
+  vDataMen.name = 'MEN';
+  vDataMen.data = vMen;
+  vDataSeries.push(vDataMen);
+}
+
+function getAllData_serie2(pQuery) {
+  var vURL    = vServer + "/api/v1/population/" + pQuery + "?apikey=" + vApiKeyOUT;
+
+  var request = $.ajax({
+     url        : vURL
+    ,type       : "GET"
+    ,data       : ""
+    ,contentType: "application/json; charset=utf-8"
+    ,cache      : false
+  });
+
+  request.done(function(data, status, jqXHR){
+    getData_serie2(data);
+    fusionarSerie1Serie2();
+    showGraph();
+  });
+}
+
+function getData_serie2(data){
+  var vPopulation   = [];
+  // inicializamos a 0 tantos valores de la serie 2 como valores tenga la serie 1
+  for (i=0 ; i<vDataSeries[0].data.length; i++ ) { vPopulation[i] = 0; }
+
+  $.each(data, function(){
+      vDataCategories_serie2.push(this.province);
+      //vPopulation.push(this.number);
+      //Sólo insertamos los valores leidos en su posición
+      //si encontramos coincidencias posicionales con las nuestras
+      var indexInSerie1 = vDataCategories_serie1.indexOf(this.province);
+      vPopulation.splice(indexInSerie1,0,this.number);
+
+  });
+
+  var vDataPopulation  = {};
+  vDataPopulation.name = 'Population';
+  vDataPopulation.data = vPopulation;
+  vDataSeries.push(vDataPopulation);
+}
+
+function fusionarSerie1Serie2() {
+  var a = 1;
+}
+
+function showGraph() {
+  Highcharts.setOptions({
+    colors: ['#0000FF', '#FF0000']
+  });
+
+  $('#container').highcharts({
+      chart: {
+          type: 'column'
+      },
+      title: {
+          text: 'Men olders than 18 years old + Number of population'
+      },
+      subtitle: {
+          text: 'Source: ine.es'
+      },
+      xAxis: {
+          categories: vDataCategories_serie1,
+          crosshair: true
+      },
+      yAxis: {
+          min: 0,
+          title: {
+              text: 'Results of integration'
+          }
+      },
+      tooltip: {
+          headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+          pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+              '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
+          footerFormat: '</table>',
+          shared: true,
+          useHTML: true
+      },
+      plotOptions: {
+          column: {
+              pointPadding: 0.2,
+              borderWidth: 0
+          }
+      },
+      series: vDataSeries
+  });
+
+}
