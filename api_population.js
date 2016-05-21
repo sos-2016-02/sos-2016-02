7ba0c0876f1ab6363eefdd75a7e1631edd4c069c
Victor Grousset/tuxayo <victor@tuxayo.net>
2016-05-21 14:58:20 +0200

    population API: disable all API key checks to prepare #106
    Comment all passport related code

--- api_population.js
+++ api_population.js
@@ -1,5 +1,5 @@
 var bodyParser = require('body-parser');
-var passport = require('passport');
+// var passport = require('passport');
 var cors = require('cors');
 var express = require('express');
 var router = express.Router();
@@ -8,23 +8,23 @@ var controller = require('./controllers/api/population.js');
 module.exports = router;
 
 router.use(bodyParser.json());
-router.use(passport.initialize());
+// router.use(passport.initialize());
 router.use(cors());
 
-router.get('/loadInitialData', controller.checkAuthentication, (req,res) => controller.loadInitialData(req, res) );
+router.get('/loadInitialData', (req,res) => controller.loadInitialData(req, res) );
 
 // on collection
-router.get('/', controller.checkAuthentication, (req, res) => controller.getPopulationData(req, res));
-router.post('/', controller.checkAuthentication, (req, res) => controller.postNewDatum(req, res));
-router.put('/', controller.checkAuthentication, (req, res) => res.sendStatus(405));
-router.delete('/', controller.checkAuthentication, (req, res) => controller.deleteAllData(req, res));
+router.get('/', (req, res) => controller.getPopulationData(req, res));
+router.post('/', (req, res) => controller.postNewDatum(req, res));
+router.put('/', (req, res) => res.sendStatus(405));
+router.delete('/', (req, res) => controller.deleteAllData(req, res));
 
 // on a subset of collection
-router.get('/:province(\\D+)/', controller.checkAuthentication, (req, res) => controller.getDataByProvince(req, res));
-router.get('/:year(\\d+)/', controller.checkAuthentication, (req, res) => controller.getDataByYear(req, res));
+router.get('/:province(\\D+)/', (req, res) => controller.getDataByProvince(req, res));
+router.get('/:year(\\d+)/', (req, res) => controller.getDataByYear(req, res));
 
 // on individual ressources
-router.get('/:province/:year', controller.checkAuthentication, (req, res) => controller.getDatum(req, res));
-router.put('/:province/:year', controller.checkAuthentication, (req, res) => controller.putDatumToUpdate(req, res));
-router.delete('/:province/:year', controller.checkAuthentication, (req, res) => controller.deleteDatum(req, res));
-router.post('/:province/:year', controller.checkAuthentication, (req, res) => res.sendStatus(405));
+router.get('/:province/:year', (req, res) => controller.getDatum(req, res));
+router.put('/:province/:year', (req, res) => controller.putDatumToUpdate(req, res));
+router.delete('/:province/:year', (req, res) => controller.deleteDatum(req, res));
+router.post('/:province/:year', (req, res) => res.sendStatus(405));
