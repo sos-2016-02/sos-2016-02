9957ca51f6f1e9e2e222cda14cc4c2bda9ebf788
Victor Grousset/tuxayo <victor@tuxayo.net>
2016-05-22 15:51:52 +0200

    add proxy for cityBikes API
    Because it's not an HTTPS API and it would have been a problem

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var api_population          = require('./api_population');
 var api_electrical_consume  = require('./api_electrical_consume');
 var api_price_of_oil        = require('./api_price_of_oil');
 var api_companiero_grupo_04  = require('./api_companiero_grupo_04');
+var api_proxy_citybikes  = require('./api_proxy_citybikes');
 
 // __ VAR -> Others __
 var bodyParser = require('body-parser');
@@ -35,6 +36,7 @@ app.use('/api/v1/population', api_population);
 app.use('/api/v1/participants-number', api_electrical_consume);
 app.use('/api/v1/oil', api_price_of_oil);
 app.use('/api/v1/population-unemployed-percentage-by-gender',api_companiero_grupo_04);
+app.use('/api/proxy/citybikes', api_proxy_citybikes);
 // __ Other parameters __
 app.use(bodyParser.json());
 app.use('/', express.static(__dirname + '/static'));
